aa29ad449a1658a3c2886be0bff1d432a297cf39
esmeepeters <esmeepeters38@gmail.com>
2025-07-08 12:34:35 +0200

    footer stap 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather app</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        <a
+          href="https://github.com/esmeep/week5-weerapp-react-"
+          target="_blank"
+        ></a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
