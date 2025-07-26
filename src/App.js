eae10b95a0e98f1cd13f39a2d910d615df1d9225
esmeepeters <esmeepeters38@gmail.com>
2025-07-26 14:39:08 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Esmee Peters and is {""}
-        <a href="https://github.com/esmeep/week5-weerapp-react-">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Esmee Peters and is {""}
+          <a href="https://github.com/esmeep/week5-weerapp-react-">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
