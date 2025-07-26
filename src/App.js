6d0bc22f3989a14ed89f7d616817bfacbdd03c4d
esmeepeters <esmeepeters38@gmail.com>
2025-07-26 13:06:24 +0200

    eddad spatie

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Esmee Peters and is{""}
+        This project was coded by Esmee Peters and is {""}
         <a href="https://github.com/esmeep/week5-weerapp-react-">
           open-sourced on GitHub
         </a>
